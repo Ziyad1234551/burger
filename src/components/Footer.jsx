a46d14718fb5ca07a1662510b394449c04e21a61
Ziyad <ziya19128@student.nurulfikri.ac.id>
2024-01-23 23:54:24 +0700

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaGithub, FaInstagram } from 'react-icons/fa6'
 
 export const Footer = () => {
   return (
-    <div  className='w-full lg:mt-[60px] absolute bg-black py-10 grid grid-cols-1 lg:grid-cols-4 gap-[40px] poppin px-4 lg:px-[100px] h-auto lg:h-[60vh] text-white'>
+    <div data-aos="fade-down" className='w-full lg:mt-[60px] absolute bg-black py-10 grid grid-cols-1 lg:grid-cols-4 gap-[40px] poppin px-4 lg:px-[100px] h-auto lg:h-[60vh] text-white'>
         <div>
             <h1 className='text-primary font-bold text-4xl '>Get In Touch</h1>
             <p className="text-white my-3 leading-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolor oluptates, sunt obcaecati quisquam nisi repellendus eum sed quos minus omnis rem asperiores magni? Quos, nihil.</p>
